064ab82aafe04a611f24c5d9b31a4b186f0ae33b
Julio Anccasi <julio.anccasi.dev@gmail.com>
2024-09-01 10:10:29 -0500

    First commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,14 @@
+
+export default function Index() {
+  return (
+    <p id="zero-state">
+      This is a demo for React Router.
+      <br />
+      Check out{' '}
+      <a href="http://reactrouter.com">
+        the docs at reactrouter.com
+      </a>
+      .
+    </p>
+  );
+}
